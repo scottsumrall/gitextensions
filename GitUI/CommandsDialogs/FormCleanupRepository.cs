edb8c0b6852347117455f13e684b6efecf148062
scottsumrall <ssumrall@elon.edu>
2023-07-13 02:11:26 -0400

    Formatted strings using gitignore formatting guidelines

--- GitUI/CommandsDialogs/FormCleanupRepository.cs
+++ GitUI/CommandsDialogs/FormCleanupRepository.cs
@@ -113,7 +113,8 @@ namespace GitUI.CommandsDialogs
             // 1. get all lines from text box which are not empty
             // 2. wrap lines with ""
             // 3. join together with space as separator
-            return string.Join(" ", textBoxExcludePaths.Lines.Where(a => !string.IsNullOrEmpty(a)).Select(a => string.Format("\"{0}\"", a)));
+
+            return string.Join(" ", textBoxExcludePaths.Lines.Where(a => !string.IsNullOrEmpty(a)).Select(a => string.Format("-e /{0}", a.Replace(" ", "?").Replace("\\", "/"))));
         }
 
         private void Close_Click(object sender, EventArgs e)
